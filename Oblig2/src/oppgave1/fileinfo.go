854681641df8aa66f2be5aa7b88061caf1d7e6d8
TobiasAlbert123 <tobiasalbert97@gmail.com>
2018-03-02 13:25:54 +0100

    Start of oppg 1 & 2

--- Oblig2/src/oppgave1/fileinfo.go
+++ Oblig2/src/oppgave1/fileinfo.go
@@ -4,28 +4,59 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"bufio"
 )
 
-func main() {
-
+func main2(input string) {
 	fmt.Println("Programmet skal returnere informasjon om en fil")
-
-	Info()
+	fileSize(input)
+	Info(input)
 }
 
-func Info() {
-	fi, err := os.Lstat("Oblig2/src/oppgave1/randomstuff.txt")
+func printAnInput () {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
+
+}
+func main () {
+
+	argsWithProg := os.Args
+	argsWithoutProg := os.Args[1:]
+
+	arg := os.Args[3]
+	fmt.Println(argsWithProg)
+	fmt.Println(argsWithoutProg)
+	fmt.Println(arg)
+
+	main2(arg)
+}
+
+func fileSize(input string) {
+	path := "Oblig2/src/files/" + input
+	fi, err := os.Stat(path)
+	if err != nil {
+
+	}
+	bytes := fi.Size()
+	kiloBytes := fi.Size()/1024
+	megaBytes := kiloBytes/1024
+	gigaBytes := megaBytes/1024
+	fmt.Printf("The file has size (rounded down):\n%d bytes\n%d KB\n%d MB\n%d GB\n\n", bytes, kiloBytes, megaBytes, gigaBytes)
+
+}
+
+func Info(input string) {
+	path := "Oblig2/src/files/" + input
+	fi, err := os.Lstat(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	mode := fi.Mode()
+	fmt.Println("The file")
 
-	if mode.IsRegular() {
-		fmt.Println("is a regular file")
-	} else {
-		fmt.Println("is not a regular file")
-	}
+	mode := fi.Mode()
 
 	if mode.IsDir() {
 		fmt.Println("is a directory")
@@ -33,17 +64,14 @@ func Info() {
 		fmt.Println("is not a directory")
 	}
 
-	if mode&os.ModeSymlink != 0 {
-		fmt.Println("is a symbolic link")
+	if mode.IsRegular() {
+		fmt.Println("is a regular file")
 	} else {
-		fmt.Println("is not a symbolic link")
+		fmt.Println("is not a regular file")
 	}
 
-	if mode&os.ModeNamedPipe != 0 {
-		fmt.Println("is a named pipe")
-	} else {
-		fmt.Println("is not a named pipe")
-	}
+	// Has Unix permission bits: -rwxrwxrwx
+
 
 	if mode&os.ModeAppend != 0 {
 		fmt.Println("is append-only")
@@ -57,30 +85,19 @@ func Info() {
 		fmt.Println("is not a device file")
 	}
 
-	/*
-	case mode.IsDir():
-		fmt.Println("directory")
-	case mode&os.ModeSymlink != 0:
-		fmt.Println("symbolic link")
-	case mode&os.ModeNamedPipe != 0:
-		fmt.Println("named pipe")*/
+	// Is/Is not a Unix character device
 
-}
+	// Is/Is not a Unix block device
 
-func mainn() {
-	fi, err := os.Lstat("Oblig2/src/oppgave1/randomstuff.txt")
-	if err != nil {
-		log.Fatal(err)
+	if mode&os.ModeSymlink != 0 {
+		fmt.Println("is a symbolic link")
+	} else {
+		fmt.Println("is not a symbolic link")
 	}
 
-	switch mode := fi.Mode(); {
-	case mode.IsRegular():
-		fmt.Println("regular file")
-	case mode.IsDir():
-		fmt.Println("directory")
-	case mode&os.ModeSymlink != 0:
-		fmt.Println("symbolic link")
-	case mode&os.ModeNamedPipe != 0:
-		fmt.Println("named pipe")
+	if mode&os.ModeNamedPipe != 0 {
+		fmt.Println("is a named pipe")
+	} else {
+		fmt.Println("is not a named pipe")
 	}
 }
--- Oblig2/src/oppgave2/filecount.go
+++ Oblig2/src/oppgave2/filecount.go
@@ -4,4 +4,27 @@ import "fmt"
 
 func main() {
 	fmt.Println("Programmet skal lese en tekst-fil, skrive ut totalt antall linjer og antall for fem runes som forekommer hyppigst i filen")
+	makeMap()
+	somevalues()
+	printtest()
+	fmt.Println(RuneMap)
+	}
+
+var RuneMap = make(map[rune]int)
+
+func makeMap() {
+	for i := 0; i < 256; i++ {
+		RuneMap[rune(i)] = 0
+	}
+}
+
+func somevalues() {
+	RuneMap[rune(98)]++
+	RuneMap[rune(110)]++
+}
+
+func printtest() {
+	for i := 0; i < 256; i++ {
+		fmt.Println(string(i), RuneMap[rune(i)])
+	}
 }
