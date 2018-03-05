c740025b70c4fc204f468243d47f6cd2a11f4025
TobiasAlbert123 <tobiasalbert97@gmail.com>
2018-03-05 16:10:44 +0100

    Update on oppgave 3

--- Oblig2/src/oppgave3/addtofile.go
+++ Oblig2/src/oppgave3/addtofile.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Oppgave 3")
+	WriteNumbers(Input())
+}
+
+func WriteNumbers(number1, number2 int) {
+	file, err := os.Create("Oblig2/src/oppgave3/testfile.txt")
+	if err != nil {
+		fmt.Println("Error creating file")
+	}
+	defer file.Close()
+	_, err = file.WriteString(fmt.Sprintf("%d\n%d\n" , number1, number2))
+	if err != nil {
+		fmt.Println("Error writing")
+	} else {
+		fmt.Printf("Wrote numbers %d and %d to testfile.txt", number1, number2)
+	}
+}
+
+func Input() (int, int) {
+	var number1 int
+	var number2 int
+	fmt.Println("Enter first number")
+	_, err := fmt.Scanf("%d", &number1)
+	fmt.Println("Enter second number")
+	_, err = fmt.Scanf("%d", &number2)
+	if err != nil {
+	}
+	return number1, number2
 }
--- Oblig2/src/oppgave3/addup.go
+++ Oblig2/src/oppgave3/addup.go
@@ -1,10 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Legg sammen to tall fra terminal og summer i en annen funksjon")
-	fmt.Println(addUp(10, 10))
+	//fmt.Println("Legg sammen to tall fra terminal og summer i en annen funksjon")
+	//fmt.Println(addUp(10, 10))
+	addUp(input())
+}
+
+func input() (int, int) {
+	var number1 int
+	var number2 int
+	fmt.Println("Enter first number")
+	_, err := fmt.Scanf("%d", &number1)
+	fmt.Println("Enter second number")
+	_, err = fmt.Scanf("%d", &number2)
+	if err != nil {
+	}
+	return number1, number2
 }
 
 func addUp(tall, tall2 int) int{
