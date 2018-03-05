255826fa580ed8ef9e93af7e3a4ced517d2ef504
TobiasAlbert123 <tobia17@student.uia.no>
2018-03-05 22:27:58 +0100

    filecount.go complete, could be done MUCH better

--- Oblig2/src/oppgave2/filecount.go
+++ Oblig2/src/oppgave2/filecount.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"bufio"
+	"io/ioutil"
+	"strings"
 )
 
 
@@ -12,54 +14,94 @@ import (
 //2. cd [repository path]/oppgave2
 //3. go run filecount.go text.txt
 func main() {
-	//takes argument from cmd line
+	countLines(getFile())
+	MapMaker(getFile())
+	//fmt.Println(RuneMap)
+	sortMap()
+}
+
+//Gets filename from run argument and adds filepath to the files folder
+func getFile() string{
 	args := os.Args
 	if len(args) == 0 {
 		fmt.Println("Argument missing")
 		fmt.Println("Type 'go run fileinfo.go text.txt'")
 	}
-
 	fileName := args[1]
-	countLines(fileName)
-
-	makeMap()
-	somevalues()
-	printtest()
-	fmt.Println(RuneMap)
+	srcFolder := "../files/"
+	filePath := srcFolder + fileName
+	return filePath
 }
 
-var srcFolder = "../files/"
-
 //counts lines in file
-func countLines(input string) {
-	file, _ := os.Open(srcFolder + input)
+func countLines(filePath string) {
+	file, _ := os.Open(filePath)
 	fileScanner := bufio.NewScanner(file)
 	lineCount := 0
 	for fileScanner.Scan() {
 		lineCount++
 	}
-	fmt.Printf("'text.txt' has %d lines", lineCount)
+	fmt.Printf("'text.txt' has %d lines\n", lineCount)
 }
 
-//similiar to hashmap in java
+//initialises map containing runes and int
 var RuneMap = make(map[rune]int)
 
-//adds values to map
-func makeMap() {
-	for i := 0; i < 256; i++ {
-		RuneMap[rune(i)] = 0
+
+//Creates map and feeds values from file into addToMap
+func MapMaker(text string) {
+	file, err := ioutil.ReadFile(text)
+	if err != nil {
+
+	}
+	fileString := string(file)
+	splitString := []string(strings.Split(fileString, ""))
+
+	for i:= 0; i < len(splitString); i++ {
+		addToMap(splitString[i])
 	}
 }
 
-//adds 1 to runes 98 and 110 in map
-func somevalues() {
-	RuneMap[rune(98)]++
-	RuneMap[rune(110)]++
+//slice of all found letters, used for easier comparisons
+var foundLetters []string
+
+//adds values to map
+func addToMap(singleLetter string) {
+	foundIt := false
+	for i:= 0; i < len(foundLetters); i++ {
+		if singleLetter == foundLetters[i] {
+			foundIt = true
+		}
+	}
+
+	//Converts string to rune to int for easier adding to RuneMap
+	runeOfString := []rune(singleLetter)
+	intOfRune := int(runeOfString[0])
+
+	//adds 1 to the rune's value in the map if found, else creates new entry with value 1
+	if foundIt == true {
+		RuneMap[rune(intOfRune)]++
+	} else {
+		RuneMap[rune(intOfRune)] = 1
+		foundLetters = append(foundLetters, singleLetter)
+	}
 }
 
-//prints runes of value i and their numbers recorded
-func printtest() {
-	for i := 0; i < 256; i++ {
-		fmt.Println(string(i), RuneMap[rune(i)])
+func sortMap() {
+	for i := 1; i < 6; i++ {
+		number := i
+		highestCount := 0
+		mostUsed := ""
+		for i := 0; i < len(foundLetters); i++ {
+			runeOfString := []rune(foundLetters[i])
+			mentions := RuneMap[runeOfString[0]]
+			if mentions > highestCount {
+				highestCount = mentions
+				mostUsed = foundLetters[i]
+			}
+		}
+		mostUsedRune := []rune(mostUsed)
+		fmt.Printf("%d. Rune: '%s' , Counts: %d\n", number, mostUsed, highestCount)
+		delete(RuneMap, mostUsedRune[0])
 	}
 }
