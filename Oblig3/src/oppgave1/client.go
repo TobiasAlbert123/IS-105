ef02196708dd8a260b6fc15caa7135785190b21a
Unknown <tobiasalbert97@gmail.com>
2018-04-08 16:26:32 +0200

    Files to be used

---
+++ Oblig3/src/oppgave1/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net"
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func main() {
+
+	//tries to open a tcp connection at port 8080
+	conn, _ := net.Dial("tcp", "localhost:8080")
+
+	//reads a new input line
+	scanner := bufio.NewReader(os.Stdin)
+
+	//shutdown with ctrl+c
+	for {
+
+		fmt.Print("Send text: ")
+
+		//reads input
+		text, _ := scanner.ReadString('\n')
+
+		//sends input over connection
+		fmt.Fprintf(conn, text + "\n")
+
+
+		message, _:= bufio.NewReader(conn).ReadString('\n')
+
+
+		fmt.Print("Message from server: " + message)
+	}
+
+}
